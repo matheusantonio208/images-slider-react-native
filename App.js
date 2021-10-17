f6215216e705bac8b6d4234adaf7c9b9e03f7914
Matheus Antonio <matheus.antonio208@hotmail.com>
2021-10-17 12:08:39 -0300

    add title and text with json object images

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ import {
 
 const {width, height} = Dimensions.get('screen');
 
-const data = [
+/*const data = [
   'https://cdn.dribbble.com/users/5438117/screenshots/16359789/media/8db00b10fc21042219498628f620a55b.jpg?compress=1&resize=1200x900',
   'https://cdn.dribbble.com/users/5438117/screenshots/16031875/media/8056f478f4ba3299ec29b98515049b55.jpg?compress=1&resize=1200x900',
   'https://cdn.dribbble.com/users/5438117/screenshots/16001836/media/8352b32189a286e0b1dd5e89e3abef19.jpg?compress=1&resize=1200x900',
@@ -21,6 +21,39 @@ const data = [
   'https://cdn.dribbble.com/users/5438117/screenshots/15518374/media/a4aa1b77cbbaf699a46b6d8e70931398.jpg?compress=1&resize=1200x900',
   'https://cdn.dribbble.com/users/5438117/screenshots/15308776/media/cfc5a5f17eeac77f5a39e2be371b19ab.jpg?compress=1&resize=1200x900',
   'https://cdn.dribbble.com/users/5438117/screenshots/13992828/media/bd87e71bf7770c808e391082d16820f7.jpeg?compress=1&resize=800x600',
+];
+*/
+const data = [
+  {
+    image:
+      'https://cdn.dribbble.com/users/5438117/screenshots/16359789/media/8db00b10fc21042219498628f620a55b.jpg?compress=1&resize=1200x900',
+    title: 'Little Goat',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra mauris eu auctor euismod. Suspendisse elit enim, posuere non volutpat non, venenatis sed ligula.',
+  },
+  {
+    image:
+      'https://cdn.dribbble.com/users/5438117/screenshots/16031875/media/8056f478f4ba3299ec29b98515049b55.jpg?compress=1&resize=1200x900',
+    title: 'Café',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra mauris eu auctor euismod. Suspendisse elit enim, posuere non volutpat non, venenatis sed ligula.',
+  },
+  {
+    image:
+      'https://cdn.dribbble.com/users/5438117/screenshots/16001836/media/8352b32189a286e0b1dd5e89e3abef19.jpg?compress=1&resize=1200x900',
+    title: 'Morning routines',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra mauris eu auctor euismod. Suspendisse elit enim, posuere non volutpat non, venenatis sed ligula.',
+  },
+  {
+    image:
+      'https://cdn.dribbble.com/users/5438117/screenshots/15890234/media/6e1e54ae3918acbf88515d622d513597.jpg?compress=1&resize=1200x900',
+    title: 'Goose',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra mauris eu auctor euismod. Suspendisse elit enim, posuere non volutpat non, venenatis sed ligula.',
+  },
+  {
+    image:
+      'https://cdn.dribbble.com/users/5438117/screenshots/15518374/media/a4aa1b77cbbaf699a46b6d8e70931398.jpg?compress=1&resize=1200x900',
+    title: 'Flowerbed',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra mauris eu auctor euismod. Suspendisse elit enim, posuere non volutpat non, venenatis sed ligula.',
+  },
 ];
 
 const imageW = width * 0.7;
@@ -45,7 +78,7 @@ export default () => {
           return (
             <Animated.Image
               key={`image-${index}`}
-              source={{uri: image}}
+              source={{uri: image.image}}
               style={[StyleSheet.absoluteFillObject, {opacity}]}
               blurRadius={50}
             />
@@ -75,15 +108,40 @@ export default () => {
               },
               shadowRadius: 20,
             }}>
-            <Image
-              source={{uri: item}}
-              style={{
-                width: imageW,
-                height: imageH,
-                resizeMode: 'cover',
-                borderRadius: 16,
-              }}
-            />
+            <View>
+              <Image
+                source={{uri: item.image}}
+                style={{
+                  width: imageW,
+                  height: imageH,
+                  resizeMode: 'cover',
+                  borderRadius: 16,
+                }}
+              />
+              <View style={{width: imageW}}>
+                <Text
+                  style={{
+                    fontFamily: 'Roboto-Black',
+                    fontSize: 36,
+                    textAlign: 'left',
+                    textTransform: 'uppercase',
+                    color: '#fff',
+                    marginTop: 20,
+                  }}>
+                  {item.title}
+                </Text>
+                <Text
+                  style={{
+                    fontFamily: 'Roboto-Regular',
+                    fontSize: 16,
+                    textAlign: 'left',
+                    color: '#fff',
+                    marginTop: 15,
+                  }}>
+                  {item.text}
+                </Text>
+              </View>
+            </View>
           </View>
         )}
       />
