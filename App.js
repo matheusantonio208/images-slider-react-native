0a03d1c1b4fb3d9921600d79ed0b26e093014743
Matheus Antonio <matheus.antonio208@hotmail.com>
2021-10-17 12:09:30 -0300

    refatoring code

--- App.js
+++ App.js
@@ -13,16 +13,6 @@ import {
 
 const {width, height} = Dimensions.get('screen');
 
-/*const data = [
-  'https://cdn.dribbble.com/users/5438117/screenshots/16359789/media/8db00b10fc21042219498628f620a55b.jpg?compress=1&resize=1200x900',
-  'https://cdn.dribbble.com/users/5438117/screenshots/16031875/media/8056f478f4ba3299ec29b98515049b55.jpg?compress=1&resize=1200x900',
-  'https://cdn.dribbble.com/users/5438117/screenshots/16001836/media/8352b32189a286e0b1dd5e89e3abef19.jpg?compress=1&resize=1200x900',
-  'https://cdn.dribbble.com/users/5438117/screenshots/15890234/media/6e1e54ae3918acbf88515d622d513597.jpg?compress=1&resize=1200x900',
-  'https://cdn.dribbble.com/users/5438117/screenshots/15518374/media/a4aa1b77cbbaf699a46b6d8e70931398.jpg?compress=1&resize=1200x900',
-  'https://cdn.dribbble.com/users/5438117/screenshots/15308776/media/cfc5a5f17eeac77f5a39e2be371b19ab.jpg?compress=1&resize=1200x900',
-  'https://cdn.dribbble.com/users/5438117/screenshots/13992828/media/bd87e71bf7770c808e391082d16820f7.jpeg?compress=1&resize=800x600',
-];
-*/
 const data = [
   {
     image:
